625444320eb464105082e9f72f0183fbf9a49288
izayaki22 <suyonpark00@naver.com>
2023-03-05 18:28:44 +0900

    feat: 로또 당첨 통계를 화면에 출력 기능 추가 - Collection 클래스의 frequency 메서드를 사용해 3개, 4개, 5개, 6개가 일치하는 로또의 개수가 각각 몇 개인지 보여줌

--- src/main/java/com/poolc/javapractice/lotto/Step1.java
+++ src/main/java/com/poolc/javapractice/lotto/Step1.java
@@ -20,6 +20,15 @@ public class Step1 {
 
         ArrayList<Integer> winList = winNumbers(win);
         ArrayList<Integer> lottoResults = countResults(winList, lotto, lottoCount);
+
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+
+        System.out.println("3개 일치 (5000원) - " + Collections.frequency(lottoResults, 3) + "개");
+        System.out.println("4개 일치 (50000원) - " + Collections.frequency(lottoResults, 4) + "개");
+        System.out.println("5개 일치 (1500000원) - " + Collections.frequency(lottoResults, 5) + "개");
+        System.out.println("6개 일치 (2000000000원) - " + Collections.frequency(lottoResults, 6) + "개");
     }
 
     private static ArrayList<Integer>[] makeLotto(int lottoCount) {
