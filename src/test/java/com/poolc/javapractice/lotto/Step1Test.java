436ed5fd25091b3736793662f9994ca6b1146a40
izayaki22 <suyonpark00@naver.com>
2023-03-05 20:16:21 +0900

    test: winNumbers 메서드의 테스트 코드 추가 - ","로 연결된 String을 잘라 넣은 ArrayList와 직접 1부터 6까지의 숫자를 집어 넣은 ArrayList가 동일한지 비교하는 테스트

--- src/test/java/com/poolc/javapractice/lotto/Step1Test.java
+++ src/test/java/com/poolc/javapractice/lotto/Step1Test.java
@@ -2,19 +2,33 @@ package com.poolc.javapractice.lotto;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
 import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class Step1Test {
 
     @Test
     public void makeLottoTest(){
-
         Step1 step1 = new Step1();
         ArrayList<Integer>[] lotto = step1.makeLotto(100);
         for (int i = 0; i < 10; i++){
             for (int j = 0; j < lotto[i].size(); j++){
-                Assertions.assertTrue(lotto[i].get(j) >= 1 && lotto[i].get(j) <= 44);
+                Assertions.assertTrue(lotto[i].get(j) >= 1 && lotto[i].get(j) <= 45);
             }
         }
+    }
 
+    @Test
+    public void winNumbersTest(){
+        Step1 step1 = new Step1();
+        ArrayList<Integer> expectedList = step1.winNumbers("1, 2,3, 4, 5,6");
+        ArrayList<Integer> actualList = new ArrayList<>();
+        for (int i = 1; i < 7; i++){
+            actualList.add(i);
+        }
+        Assertions.assertEquals(expectedList, actualList);
     }
 }
