59b6894f216188b9ed6f6159d88132206d34e407
izayaki22 <suyonpark00@naver.com>
2023-03-05 21:04:37 +0900

    test: countResults 메서드의 테스트 코드 추가 - 임의로 정한 당첨 번호와 두 개의 로또번호를 인수로 입력하여 countResults 메서드로 반환된 ArrayList와, 직접 일치하는 번호의 개수를 세어 만든 ArrayList가 동일한지 비교하는 테스트

--- src/test/java/com/poolc/javapractice/lotto/Step1Test.java
+++ src/test/java/com/poolc/javapractice/lotto/Step1Test.java
@@ -2,19 +2,48 @@ package com.poolc.javapractice.lotto;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
 import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class Step1Test {
 
     @Test
     public void makeLottoTest(){
-
         Step1 step1 = new Step1();
-        ArrayList<Integer>[] lotto = step1.makeLotto(100);
+        ArrayList<ArrayList<Integer>> lotto = step1.makeLotto(100);
         for (int i = 0; i < 10; i++){
-            for (int j = 0; j < lotto[i].size(); j++){
-                Assertions.assertTrue(lotto[i].get(j) >= 1 && lotto[i].get(j) <= 44);
+            for (int j = 0; j < lotto.get(i).size(); j++){
+                Assertions.assertTrue(lotto.get(i).get(j) >= 1 && lotto.get(i).get(j) <= 45);
             }
         }
+    }
 
+    @Test
+    public void winNumbersTest(){
+        Step1 step1 = new Step1();
+        ArrayList<Integer> actualList = step1.winNumbers("1, 2,3, 4, 5,6");
+        ArrayList<Integer> expectedList = new ArrayList<>();
+        for (int i = 1; i < 7; i++){
+            expectedList.add(i);
+        }
+        Assertions.assertEquals(actualList, expectedList);
+    }
+
+    @Test
+    public void countResultsTest(){
+        Step1 step1 = new Step1();
+        ArrayList<Integer> winListTest = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        ArrayList<ArrayList<Integer>> lottoTest = new ArrayList<>(2);
+        lottoTest.add(new ArrayList<>(Arrays.asList(3, 4, 5, 6, 7, 8)));
+        lottoTest.add(new ArrayList<>(Arrays.asList(4, 5, 6, 7, 8, 9)));
+        ArrayList<Integer> actualList = step1.countResults(winListTest, lottoTest, 2);
+
+        ArrayList<Integer> expectedList = new ArrayList<>();
+        expectedList.add(4);
+        expectedList.add(3);
+        Assertions.assertEquals(actualList, expectedList);
     }
 }
