026b3574f881dc18a1c6d32fdc945931e55374da
izayaki22 <suyonpark00@naver.com>
2023-03-05 17:54:11 +0900

    feat: 로또 구입 금액을 입력 받는 기능 추가 - Scanner를 이용하여 사용자가 직접 구입 금액을 입력하도록 함 - 이후 지난 주 당첨 번호도 입력 받아야되기에,  nextLine을 통해 입력받은 로또 구입 금액 뒤에 붙어있는 "\n"을 지움

---
+++ src/main/java/com/poolc/javapractice/lotto/Step1.java
@@ -0,0 +1,15 @@
+package com.poolc.javapractice.lotto;
+
+import java.util.*;
+
+public class Step1 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("구입금액을 입력해 주세요.");
+        int buyAmount = sc.nextInt();
+        sc.nextLine();
+
+    }
+}
+
