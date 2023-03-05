1d18228880311fc08f06644a1ad3dd14265a47a2
izayaki22 <suyonpark00@naver.com>
2023-03-05 18:02:56 +0900

    feat: 구매한 로또 개수만큼 로또를 랜덤으로 생성하는 기능 추가 - makeLotto 메서드는 로또 개수를 입력받고, 생성한 로또를 2차원의 ArrayList로 만들어 반환 - 로또 번호는 1부터 45까지 중복없는 6개의 숫자를 랜덤으로 고름 - 하나의 로또를 생성하면 해당 로또를 화면에 출력함

--- src/main/java/com/poolc/javapractice/lotto/Step1.java
+++ src/main/java/com/poolc/javapractice/lotto/Step1.java
@@ -3,7 +3,6 @@ package com.poolc.javapractice.lotto;
 import java.util.*;
 
 public class Step1 {
-
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("구입금액을 입력해 주세요.");
@@ -13,7 +12,26 @@ public class Step1 {
         int lottoCount = buyAmount / 1000;
         System.out.println(lottoCount + "개를 구매했습니다.");
 
+        ArrayList<Integer>[] lotto = makeLotto(lottoCount);
+        System.out.println();
+    }
 
+    private static ArrayList<Integer>[] makeLotto(int lottoCount) {
+        ArrayList<Integer>[] lotto = new ArrayList[lottoCount];
+        Random random = new Random();
+        for (int i = 0; i < lottoCount; i++) {
+            lotto[i] = new ArrayList<>();
+
+            while (lotto[i].size() < 6) {
+                int num = random.nextInt(44) + 1;
+                if (!lotto[i].contains(num)) {
+                    lotto[i].add(num);
+                }
+            }
+
+            System.out.println(lotto[i].toString());
+        }
+        return lotto;
     }
 }
 
