1681bd54b9fa700d8b84fe7bc9fe63ed56eec982
izayaki22 <suyonpark00@naver.com>
2023-03-07 21:52:49 +0900

    refactor: 당첨 통계를 출력할 때 3개, 4개, 5개, 6개가 일치하는 로또의 개수를 불러오는 방법 수정 - Collections.frequency 메서드가 중복으로 사용 되는 것을 줄이고자 result 메서드로 반환된 resultList의 원소를 앞에서부터 하나씩 가져오는 방법으로 수정

--- src/main/java/com/poolc/javapractice/lotto/Output.java
+++ src/main/java/com/poolc/javapractice/lotto/Output.java
@@ -7,14 +7,14 @@ public class Output {
         System.out.println(lottoCount + "개를 구매했습니다.");
     }
 
-    public void printWinStatistics(ArrayList<Integer> lottoResults) {
+    public void printWinStatistics(ArrayList<Integer> resultList) {
         System.out.println("당첨 통계");
         System.out.println("---------");
 
-        System.out.println("3개 일치 (5000원) - " + Collections.frequency(lottoResults, 3) + "개");
-        System.out.println("4개 일치 (50000원) - " + Collections.frequency(lottoResults, 4) + "개");
-        System.out.println("5개 일치 (1500000원) - " + Collections.frequency(lottoResults, 5) + "개");
-        System.out.println("6개 일치 (2000000000원) - " + Collections.frequency(lottoResults, 6) + "개");
+        System.out.println("3개 일치 (5000원) - " +  resultList.get(0) + "개");
+        System.out.println("4개 일치 (50000원) - " + resultList.get(1) + "개");
+        System.out.println("5개 일치 (1500000원) - " + resultList.get(2) + "개");
+        System.out.println("6개 일치 (2000000000원) - " + resultList.get(3) + "개");
     }
 
     public void printEarningRate(double earningRate) {
